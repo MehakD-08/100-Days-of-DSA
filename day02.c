9bb6831847c5e36d57da237faddbe8ed71b4aa2c
Mehak Dilawar <mehakdilawar8@gmail.com>
2026-02-03 14:42:50 +0530

    day02.c

--- day02.c
+++ day02.c
@@ -21,14 +21,14 @@ int main(){
         printf("Enter element: ");
         scanf("%d", &arr[i]);
     }
-    printf("Enter position to delete array from: ");
+    printf("\nEnter position to delete element: ");
     scanf("%d", &pos);
-    for(int i=pos-1; i>n-1; i--){
+    for(int i=pos-1; i<n; i++){
         arr[i]=arr[i+1];
     }
-    for(int i=0; i<n-1; i++){
+    for(int i=0; i<n; i++){
         printf("%d ", arr[i]);
-        
     }
 return 0;
+
 }
