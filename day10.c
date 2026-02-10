e24a663a07a1f2784135a9c20a1bff8cad21d163
Mehak Dilawar <mehakdilawar8@gmail.com>
2026-02-10 23:56:40 +0530

    day10.c

--- day10.c
+++ day10.c
@@ -1,1 +1,13 @@
+//Problem: Read a string and check if it is a palindrome using two-pointer comparison.
+/*
+Example:
+Input:
+level
 
+Output:
+YES
+*/
+#include<stdio.j>
+int main(){
+  char s[1000];
+  printf("Enter string:")
