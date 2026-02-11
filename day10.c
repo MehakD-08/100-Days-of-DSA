83a806552a9a9d05919cd62f5afda73ca3263e96
Mehak Dilawar <mehakdilawar8@gmail.com>
2026-02-11 11:22:20 +0530

    day10.c

--- day10.c
+++ day10.c
@@ -7,8 +7,24 @@ level
 Output:
 YES
 */
-#include<stdio.j>
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
 int main(){
   char s[1000];
-  printf("Enter string:")
+  printf("Enter string:");
   scanf("%s", &s);
+  int left=0;
+  int right=strlen(s);
+  bool isPalindrome= true;
+  while(left<right){
+   if(s[left]!=s[right-1]){
+     isPalindrome= false;
+     break;
+   }
+   left++;
+   right--;
+  }
+  printf("%s\n", isPalindrome ? "YES" : "NO");
+return 0;
+}
